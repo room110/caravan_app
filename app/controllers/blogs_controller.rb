8838aff2214e33189d8f41aa53b58f41ad673674
yukiito <ito.room.room.110@gmail.com>
2020-05-20 17:37:15 +0900

    [Finish]4-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,6 +5,11 @@ class BlogsController < ApplicationController
   def show
   end
 
+  def index
+    # 記事を全件取得
+    @blogs = Blog.all
+  end
+
   def new
   	  @blog = Blog.new
   end
@@ -17,6 +22,8 @@ class BlogsController < ApplicationController
 
   def edit
   end
+
+
 
   private
     def blog_params
